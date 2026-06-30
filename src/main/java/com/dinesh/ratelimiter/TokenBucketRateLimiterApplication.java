08836d4bcef086aad7dec532758f9cf67e47e933
dineshdhayfule <dineshdhayfule@gmail.com>
2026-06-30 22:27:07 +0530

    setup

---
+++ src/main/java/com/dinesh/ratelimiter/TokenBucketRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.ratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenBucketRateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenBucketRateLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinesh/ratelimiter/TokenBucketRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenBucketRateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
