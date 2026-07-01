87bf18edddfa8789c5b784e4ec0bcb08107befa3
dineshdhayfule <dineshdhayfule@gmail.com>
2026-07-01 18:01:11 +0530

    feat: implement token bucket algorithm

---
+++ src/main/java/com/dinesh/ratelimiter/algorithm/TokenBucketRateLimiter.java
@@ -0,0 +1,55 @@
+package com.dinesh.ratelimiter.algorithm;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.dinesh.ratelimiter.model.Bucket;
+
+public class TokenBucketRateLimiter {
+
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public boolean allowRequest(Bucket bucket) {
+
+        lock.lock();
+
+        try {
+
+            refill(bucket);
+
+            if (bucket.getAvailableTokens() > 0) {
+
+                bucket.setAvailableTokens(bucket.getAvailableTokens() - 1);
+
+                return true;
+            }
+
+            return false;
+
+        } finally {
+
+            lock.unlock();
+
+        }
+    }
+
+    private void refill(Bucket bucket) {
+
+        long currentTime = System.currentTimeMillis();
+
+        long timeElapsed = currentTime - bucket.getLastRefillTimestamp();
+
+        long tokensToAdd = (timeElapsed * bucket.getRefillTokensPerSecond()) / 1000;
+
+        if (tokensToAdd > 0) {
+
+            long newTokenCount = Math.min(
+                    bucket.getAvailableTokens() + tokensToAdd,
+                    bucket.getCapacity());
+
+            bucket.setAvailableTokens(newTokenCount);
+
+            bucket.setLastRefillTimestamp(currentTime);
+        }
+    }
+
+}
--- src/main/java/com/dinesh/ratelimiter/model/Bucket.java
+++ src/main/java/com/dinesh/ratelimiter/model/Bucket.java
@@ -1,5 +1,42 @@
 package com.dinesh.ratelimiter.model;
 
+
 public class Bucket {
 
+    private final long capacity;
+    private final long refillTokensPerSecond;
+
+    private long availableTokens;
+    private long lastRefillTimestamp;
+
+    public Bucket(long capacity, long refillTokensPerSecond) {
+        this.capacity = capacity;
+        this.refillTokensPerSecond = refillTokensPerSecond;
+        this.availableTokens = capacity;
+        this.lastRefillTimestamp = System.currentTimeMillis();
+    }
+
+    public long getCapacity() {
+        return capacity;
+    }
+
+    public long getRefillTokensPerSecond() {
+        return refillTokensPerSecond;
+    }
+
+    public long getAvailableTokens() {
+        return availableTokens;
+    }
+
+    public void setAvailableTokens(long availableTokens) {
+        this.availableTokens = availableTokens;
+    }
+
+    public long getLastRefillTimestamp() {
+        return lastRefillTimestamp;
+    }
+
+    public void setLastRefillTimestamp(long lastRefillTimestamp) {
+        this.lastRefillTimestamp = lastRefillTimestamp;
+    }
 }
